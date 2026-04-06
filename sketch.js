5231b1745e0dd052e5200d5de2761f8194520e32
Lisa-Marie <lisa-marie.peischl@hfg-gmuend.de>
2026-04-06 22:59:52 +0200

    3

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+let countries
+let investments
+let AImodels
+
+function preload() {
+    // data aufrufen
+}
+
+function setup () {
+    createCanvas(1000, 1000);
+
+}
+
+function draw () {
+    
+}
