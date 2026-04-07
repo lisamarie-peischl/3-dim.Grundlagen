97dfda91ffee51ad8c615acd86ad9ac610ec27e0
Lisa-Marie <lisa-marie.peischl@hfg-gmuend.de>
2026-04-07 14:18:41 +0200

    AI models

--- AI_models.js
+++ AI_models.js
@@ -1,6 +1,35 @@
-
 class AI_models {
     constructor () {
-        
+        this.myCode = "";
+        this.myColor = color(128);
+        this.arrayOfData = [];
+        this.myWidth = 150;
+    }
+    setData(data) {
+        this.arrayOfData = data;
+    }
+
+    setColor(c) {
+        this.myColor = c;
+    }
+
+    setWidth(w) {
+        this.myWidth = w;
+    }
+
+    draw(centerX, centerY) {
+        const n = this.arrayOfData.length;
+        const maxValue = Math.max(...this.arrayOfData);
+
+        for (let i = 0; i < n; i++) {
+            const angle = (i / n) * Math.PI * 2;
+            const length = (this.arrayOfData[i] / maxValue) * this.myWidth;
+
+            const x = centerX + Math.cos(angle) * length;
+            const y = centerY + Math.sin(angle) * length;
+
+            stroke(this.myColor);
+            line(centerX, centerY, x, y);
+        }
     }
 }
